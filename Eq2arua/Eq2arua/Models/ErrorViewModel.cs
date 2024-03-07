202afb3e92fae930188bed294d7fa9fb76fb1f84
DUGEYM <142479269+DUGEYM@users.noreply.github.com>
2024-03-07 15:10:43 +0000

    project for root

---
+++ Eq2arua/Eq2arua/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eq2arua.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
